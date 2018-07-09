dbe334850abbc6c1fa39ea8ee27aa1c62c013f01
Matin Kaboli <matinkaboli@aol.com>
2018-07-09 15:32:22 +0430

    problem 9

---
+++ 9/index.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int pythagorean(int num) {
+    for (int i = 1; i < num; i++) {
+        for (int j = 1; j < num - 1; j++) {
+            const int k = num - i - j;
+            if ((i * i) + (j * j) == (k * k)) {
+                return i * j * k;
+            }
+        }
+    }
+    return 0;
+}
+int main() {
+    printf("%d\n", pythagorean(1000));
+}
